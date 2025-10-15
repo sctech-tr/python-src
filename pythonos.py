a78c06754a1d05c054071b2f50d1179f8058c001
sctech <sctech@sctech.qzz.io>
2025-10-15 18:52:17 +0300

    domain trouble (ughhhhhhhhhhhhhhhhhhh)

--- pythonos.py
+++ pythonos.py
@@ -17,7 +17,7 @@ class PythonOS:
             "pythonosfetch": self.pythonosfetch,
             "uac": self.uac
         }
-        self.appstore_url = "https://sctech.netlify.app/pythonos_appstore.json"
+        self.appstore_url = "https://box.sctech.qzz.io/pythonos_appstore.json"
         self.app_folder = "./pythonos_apps/"
         os.makedirs(self.app_folder, exist_ok=True)
 
